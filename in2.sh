eb548642f1479e914e86a06624f08fce38e88209
user <user@host>
2024-05-15 23:49:47 +0200

    .

---
+++ in2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# a simple list commmands to set the nsa-plymouth as the default boot splash screen theme
+# change "nsa-plymouth" to any other custom theme you wish to install
+# INSTALLATION INSTRUCTIONS:
+# make install.sh executable and run it from the terminal
+
+# make a new directory for storing the theme
+sudo mkdir /usr/share/plymouth/themes/nsa-plymouth
+
+# copy the contents
+sudo cp --recursive {images,nsa.plymouth,nsa.script,README.md} /usr/share/plymouth/themes/nsa-plymouth
+
+# set as default theme
+
